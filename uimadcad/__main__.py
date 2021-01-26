3ea199d08db7f1dbd102ecc7c32537c4a753ec9e
jimy byerley <jimy.byerley@gmail.com>
2021-01-26 15:40:33 +0100

    put initial file open in a QTimer to allow some interface interaction at that point

--- uimadcad/__main__.py
+++ uimadcad/__main__.py
@@ -3,7 +3,7 @@ if __name__ == '__main__':
 	from uimadcad.gui import *
 
 	import sys, os, locale
-	from PyQt5.QtCore import Qt
+	from PyQt5.QtCore import Qt, QTimer
 	from PyQt5.QtWidgets import QApplication
 	# set Qt opengl context sharing to avoid reinitialization of scenes everytime, (this is for pymadcad display)
 	QApplication.setAttribute(Qt.AA_ShareOpenGLContexts, True)
@@ -20,9 +20,13 @@ if __name__ == '__main__':
 	
 	# start software
 	madcad = Madcad()
-	if len(sys.argv) >= 2 and os.path.exists(sys.argv[1]):
-		madcad.open_file(sys.argv[1])
-	
 	main = MainWindow(madcad)
+	
+	def startup():
+		if len(sys.argv) >= 2 and os.path.exists(sys.argv[1]):
+			madcad.open_file(sys.argv[1])
+		madcad.active_sceneview.adjust()
+	
+	QTimer.singleShot(100, startup)
 	main.show()
 	sys.exit(app.exec())
