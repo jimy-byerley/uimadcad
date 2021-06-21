023cf4736f6041cbe55f438a63c8714c45ea28a4
jimy byerley <jimy.byerley@gmail.com>
2021-06-21 19:29:26 +0200

    fixes for windows build

--- install.sh
+++ install.sh
@@ -62,11 +62,12 @@ $project/launcher/target/release/pack$binformat $data/uimadcad \
 		uimadcad/__init__.py \
 		uimadcad/__main__.py
 
+install $project/launcher/uimadcad.py $bin/madcad
+	
 # platform specific
 case $platform in
 linux)
-	# launcher, must be in release mode to not contain the source code
-	install $project/launcher/madcad-linux.py $bin/madcad
+	# NOTE launcher, must be in release mode to not contain the source code
 	install $project/launcher/target/release/liblauncher.so $data/launcher.so
 
 	install -d $prefix/share/applications/
@@ -80,8 +81,7 @@ linux)
 	#update-mime-database ~/.local/share/mime
 	;;
 windows)
-	# launcher, must be in release mode to not contain the source code
-	install $project/launcher/uimadcad-windows.py $bin/uimadcad.py
+	# NOTE launcher, must be in release mode to not contain the source code
 	install $project/launcher/madcad.bat $bin/
 	install $project/launcher/target/release/launcher.dll $data/launcher.pyd
 	
