cfef0df57a011e7d6236c167b4663f85d3022514
jimy-byerley <jimy.byerley@gmail.com>
2022-05-17 20:12:45 +0200

    improved packaging tools

--- distribute.sh
+++ distribute.sh
@@ -4,7 +4,7 @@ version=0.5
 target=$(dirname $0)
 
 
-while getopts "a:h:" arg
+while getopts "p:a:h:" arg
 do
 	case $arg in 
 	a)
@@ -13,8 +13,13 @@ do
 	h)
 		platform=$OPTARG
 		;;
-	?)
-		echo "usage:  $(basename $0) (deb|tar) [-a ARCH] [-h PLATFORM]"
+	p)
+		format=$OPTARG
+		;;
+	*)
+		echo "buid uimadcad and make an installation package out of it"
+		echo
+		echo "usage:  $(basename $0) [-p deb|tar] [-a ARCH] [-h PLATFORM] (deb|tar)"
 		exit 1
 		;;
 	esac
@@ -22,7 +27,7 @@ done
 
 set -x
 
-format=$1
+format=${format:-tar}
 arch=${arch:-$(arch)}
 platform=${platform:-linux}
 prefix=$target/dist/${platform}_${arch}
--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #!/bin/sh -eu
 
-project=$(dirname $0)
-
-
 while getopts "p:a:h:" arg; do
 	case $arg in 
 	a)
@@ -14,7 +11,9 @@ while getopts "p:a:h:" arg; do
 	p)
 		prefix=$OPTARG
 		;;
-	?)
+	*)
+		echo "build uimadcad and install it at the specified prefix path"
+		echo
 		echo "usage:  $(basename $0) [-p PREFIX] [-a ARCH] [-h PLATFORM]"
 		exit 1
 		;;
@@ -23,9 +22,12 @@ done
 
 set -x
 
+project=$(realpath $(dirname $0))
 arch=${arch:-$(arch)}
 platform=${platform:-linux}
 prefix=${prefix:-$project/dist/${platform}_${arch}}
+release=release 
+# NOTE launcher must be in release mode to not contain the source code
 
 
 case $platform in
@@ -49,16 +51,25 @@ esac
 
 
 # compile the launcher
+host=$(uname)
+if [ "$(arch)" = "$arch" && "$platform" = "${host,,}" ] 
+then	cargotarget=
+fi
 (
 	cd launcher
 	cargo build --release
 
-	if [ $(arch) = $arch ];  
-	then	cargoplatform=target
-	else	cargo build --release --target $cargotarget
+	if [ -n "$cargotarget" ]
+	then	
+		export PYO3_CROSS_PYTHON_VERSION=3.9
+		export PYO3_CROSS_INCLUDE_DIR="$project/../cross/python-$PYO3_CROSS_PYTHON_VERSION-$arch/include"
+		export PYO3_CROSS_LIB_DIR="$project/../cross/python-$PYO3_CROSS_PYTHON_VERSION-$arch/lib"
+		#$project/../cross/setup.sh $PYO3_CROSS_PYTHON_VERSION $arch
+		cargo build --release --target $cargotarget
 	fi
 )
 
+# prepare directories
 install -d $bin
 install -d $data
 # main archive
@@ -78,13 +89,14 @@ $project/launcher/target/release/pack$binformat $data/uimadcad \
 		uimadcad/gui.py \
 		uimadcad/__main__.py
 
+# the main executable
 install $project/launcher/uimadcad.py $bin/madcad
 	
 # platform specific
 case $platform in
 linux)
 	# NOTE launcher, must be in release mode to not contain the source code
-	install $project/launcher/target/$cargotarget/release/liblauncher.so $data/launcher.so
+	install $project/launcher/target/$cargotarget/$release/liblauncher.so $data/launcher.so
 
 	install -d $prefix/share/applications/
 	install madcad.desktop $prefix/share/applications/
@@ -97,9 +109,8 @@ linux)
 	#update-mime-database ~/.local/share/mime
 	;;
 windows)
-	# NOTE launcher, must be in release mode to not contain the source code
 	install $project/launcher/madcad.bat $bin/
-	install $project/launcher/target/release/launcher.dll $data/launcher.pyd
+	install $project/launcher/target/$cargotarget/$release/launcher.dll $data/launcher.pyd
 	
 	install -d $prefix/icons/hicolor/scalable/apps
  	install icons/*.svg $prefix/icons/hicolor/scalable/apps/
